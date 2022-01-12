72fc60578a5ef1165d24e6b49e346efa8283b238
Israel Philip Linhares <93551089+IsraelLinhares@users.noreply.github.com>
2022-01-12 10:10:19 -0300

    exercicio 7 adicionado
    cria a media em 3 numeros

---
+++ ex007.py
@@ -0,0 +1,4 @@
+nota1 = int(input('digite sua nota:'))
+nota2 = int(input('digite sua segunda nota:'))
+nota3 = int(input('digite sua terceira nota:'))
+print('A media da sua nota é:', (nota1 + nota2 + nota3)/3)
